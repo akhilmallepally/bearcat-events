fab98388d5c5bab32d30278e991182452338a598
akhilmallepally <akhilnani831@gmail.com>
2021-10-12 01:37:42 -0500

    chore: update repo files

--- bearcatEback/bearcatE/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import bearcatE
-
-class bearcatE_admin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-# Register your models here.
-
-admin.site.register(bearcatE, bearcatE_admin)
-
--- bearcatEback/bearcatE/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import bearcatE
-
-class bearcatE_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = bearcatE
-        fields = ('id', 'title', 'description', 'completed')
