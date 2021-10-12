a4ed4be2970a4a0524e1be175e11c61bc97e45ee
akhilmallepally <akhilnani831@gmail.com>
2021-10-12 12:00:01 -0500

    added card component

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
+import PostCard from "../components/Card"
 
 const Index = () => {
   return (
     <div>
       <Header />
       <p className="text-3xl">Index page</p>
+      <PostCard />
     </div>
   )
 }
