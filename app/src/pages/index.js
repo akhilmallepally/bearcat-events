3f465b25ca7c0c3a978c670324470f3d1f9ebc9c
Akhil Mallepally <akhilnani831@gmail.com>
2021-10-27 08:52:02 -0500

    added footer in index file

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,8 +5,10 @@ const Index = () => {
   return (
     <div>
       <Header />
-      <p className="text-3xl">Index page</p>
       <PostCard />
+    <div>
+      <Footer />
+      </div>
     </div>
   )
 }
