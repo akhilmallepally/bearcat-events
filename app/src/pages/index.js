ac31b08bf76ca4448bf925341c209049175bf94e
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2021-11-08 08:34:33 -0600

    imported footer from components

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import PostCard from "../components/Card"
+import Footer from "../components/Footer"
 import Head from "next/head"
 
 const Index = () => {
