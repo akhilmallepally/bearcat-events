a3f3779a41a188ac8f3fefeed84b62ff7035e7c1
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2021-11-08 08:35:41 -0600

    added links to footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -2,7 +2,14 @@ const Footer = () => {
   return (
     
 <div className="footer">
-  <p>Footer</p>
+
+  <a href="https://github.com/kushalkatari/Proposal5B-BearcatE">Proposal</a> &nbsp;&nbsp;
+
+  <a href="https://github.com/akhilmallepally/bearcat-events">Source</a>&nbsp;&nbsp;
+
+  <a href="https://github.com/akhilmallepally/bearcat-events/projects/1">Project board</a>&nbsp;&nbsp;
+
+  <a href="">Team 5B</a>
 </div>
 
   )
